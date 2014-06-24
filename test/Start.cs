c300704d573f5fa089502386efff1b6bda342ed6
Jacques Lucke <Jacques@moonicfx.com>
2014-06-24 17:10:56 +0200

    Erstes Einchecken des Moduls test

---
+++ test/Start.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test
+{
+	public class Start
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hallo");
+			Console.ReadLine ();
+			Console.WriteLine ("sdg");
+			Console.ReadKey ();
+		}
+	}
+}
+
